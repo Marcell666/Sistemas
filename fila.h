c99c4bc77e13a88996424ade3224963dd69c9ce8
Marcell666 <brucemarcellino@hotmail.com>
2017-10-27 20:07:21 -0200

    posEntrega varias mudancas
    coloquei uma flag para termino
    movi o codigo da funcao cria novo processo para a funcao principal
    movi tambem os codigos que lidavam com um processo que termina e com processo que solicitar io para o principal
    coloquei dois variaveis a mais na fila, uma para guardar o tempo que o processo pediu io
    e outra para guardar o index da propria fila, na fila.
    corrigi um bug na funcao reset.
    mudei a estrutura do codigo na funcao princial. Retirei repetições e condicionais entrelaçadas.
    
    Estou tentando uma abordagem mais focada no proprio ut, assim qualquer evento que aconteça simplesmente aciona uma flag, para depois em algum ut ser tratado pela funcao principal, isso eveita bugs relacionados a função ser interrompiada e o controle voltar para a main ao inves da função chamada.

--- fila.h
+++ fila.h
@@ -1,13 +1,16 @@
 typedef struct fila Fila;
 typedef Fila* ptFila;
 
-ptFila FILA_cria(int tempo);
+ptFila FILA_cria(int tempo, int index);
 void FILA_insere(ptFila fila, int id, int tempoAtual);
 void FILA_remove(ptFila fila);
 void FILA_comecaIO(ptFila deFila, ptFila paraFila, ptFila filaIO, int tempoAtual);
 void FILA_atualizaIO(ptFila fila, int tempoAtual);
 int FILA_comecaCPU(ptFila fila, int tempoAtual);
 int FILA_tempoRestante(ptFila fila, int tempoAtual);
+int FILA_getTempoComecoIO(ptFila fila);
+int FILA_setTempoComecoIO(ptFila fila, int tempo);
+int FILA_getIndex(ptFila fila);
 int FILA_topId(ptFila ptFila);
 int FILA_vazia(ptFila fila);
 void FILA_limpa(ptFila fila);
--- mainF.c
+++ mainF.c
@@ -15,11 +15,16 @@
 #define MAX_ARGS  10
 #define MAX_STRING 80
 #define TRUE 1
+#define NFILAS 3
+#define CHAVE 8180
 
 /*Podiamos criar uma estrutura pra cada processo com seu pid, tempo de i/o e fila que pertence, assim conseguimos colocar ele em uma fila de i/o saber quanto tempo ele tem que ficar lá e a hora de retirar, alem disso podemos guardar o tempo de execução que ele fez (já está na fila) */
 
 ptFila filaIO;
-ptFila f1;
+ptFila filaAtual;
+ptFila f1,f2,f3;
+ptFila filas[3];
+
 
 ptFila fAtual();
 ptFila fReset();
@@ -29,39 +34,90 @@ void criaNovoProcesso(ptFila f, char *comando);
 void processoTermina(int sinal);
 void processoIO(int sinal);
 
+void encerra(int status);
+
 int tempo;
 int solicitouIO;
+int solicitouTermino;
+int indexAtual;
+int* flag;
+int segmento;
 
 int main(int argc, char **argv){
 	int tempoRestante;
 	int id;
-	int segmento;
-	int* flag;
-	int aux;
 	char comando[81];
+
+	/*funcao cria*/
+	//int id=0;
+	int i=1;
+	char arg[MAX_STRING];
+	char *args[MAX_ARGS];
+	
+	f1 = FILA_cria(1, 0);
+	f2 = FILA_cria(2, 1);
+	f3 = FILA_cria(4, 2);
+	filaIO = FILA_cria(666, 3);
+	
+	filaAtual = f1;
+	
+	filas[0] = f1;
+	filas[1] = f2;
+	filas[2] = f3;
 	
 	tempo=0;
 	solicitouIO=0;
+	indexAtual = 0;
 
 	// aloca a memória compartilhada
-	segmento = shmget (8182, sizeof (int), S_IRUSR);	
+	segmento = shmget (CHAVE, sizeof (int), S_IRUSR);	
 	// associa a memória compartilhada ao processo
 	flag = (int*) shmat (segmento, 0, 0);
-
-	f1 = FILA_cria(2);
-	filaIO = FILA_cria(666);
 
 	signal(SIGUSR1, processoIO);
 	signal(SIGUSR2, processoTermina);
 	
 	/* Loop para criar os processos */
-	//criaNovoProcesso(f1, "programa 1 3");
-	criaNovoProcesso(f1, "programa 2 3");
 
-	//criaNovoProcesso(f1, "programa 2 4 5");
-	//criaNovoProcesso(f1, "programa 3 1 2");
-	//criaNovoProcesso(f1, "programa 1 2 1");
-	id = FILA_topId(fAtual());
+	while (*flag>0) //tem coisa pra ler
+	{
+		read(0, comando, 81);
+		//printf("comando lido inicio:%s\n", comando);
+		if(strcmp(comando,"done")==0) break;
+		//criaNovoProcesso(f1, comando);
+
+		/*
+			Trazer o codigo da funcao 'criaNovoProcesso' para ca foi a unica maneira que encontrei de me prevenir de um bug
+			O que acontece e que quando uma funcao é interrompida, depois ela nao continua de onde parou, ao inves disso ela continua da main.
+			Entao se o fork que vai usar a execl para se transformar no processo criado for parado no meio do caminho, depois ele não vai conseguir continuar, e teremos um processo fantasma, que não está executando normalmente.
+			Mas se o codigo estiver aqui, mesmo que seja parado entre o printf e a chamada para execl, depois vai continuar do mesmo lugar, assim o bug não acontece (eu espero).		
+		*/
+
+		strcpy(arg, comando);
+		args[0] = strtok(arg, " ");
+		while(TRUE){
+			args[i] = strtok(NULL, " ");
+			if(args[i]==NULL) break;
+			i++;
+		}
+		id = fork();
+		if(id==0){
+			printf("%d cria: %d - tempo %d - Novo processo\n%s\n", getppid(), getpid(), tempo, comando);
+			execv("./programa", args);
+			printf("dafuq");
+			encerra(1);
+		}	
+		FILA_insere(f1,id, 0);
+		printf("getpid %d parando processo %d\n", getpid(), id);
+		if(id>0)
+			kill(id, SIGSTOP);
+		else{
+			printf("BUG AQUI\n");
+		}
+
+		*flag-=1;			
+	}
+	id = FILA_topId(f1);
 	printf("Iniciando processo %d\n", id);
 	kill(id, SIGCONT);
 
@@ -69,96 +125,90 @@ int main(int argc, char **argv){
 	while(TRUE){
 		sleep(1);
 		tempo++;
-		if (*flag==1) //tem coisa pra ler
-		{
-			aux=read(0, comando,81);
-			criaNovoProcesso(f1, comando);
-			*flag=0;			
-		}
-		printf("passou 1 u.t. agora estamos em %d\n", tempo);
+		printf("passou 1 u.t. agora estamos em %d, e na fila %d\n", tempo, indexAtual+1);
 
+		/* Atualiza os processos em I/O	*/
 		if(!FILA_vazia(filaIO))
 			FILA_atualizaIO(filaIO, tempo);
-		if(FILA_vazia(fAtual()))
-			continue;
-
-
+				
 		id = FILA_topId(fAtual());
+		printf("id= %d \n",id);
+		if(id<=0){
+			printf("fila atual vazia, procurando outro processo\n");
+			if(fReset() == NULL){
+				/*Se f reset retorna null então as tres filas estão vazias, e a fila de io tem alguem esperando IO*/
+				printf("processo aguardando I/O\n");
+				continue;
+			}
+		}
 		tempoRestante = FILA_tempoRestante(fAtual(), tempo);
 		printf("tempoRestante:%d do processo %d\n", tempoRestante, id);
-	
-		/* Nem o tempo acabou, nem o programa solicitou I/O, então basta continuar executando */
-		if(!solicitouIO && tempoRestante>=0)
-			continue;
 
-
-		if(id>0)
-			kill(id, SIGSTOP);
-		else{
-			printf("BUG NOUTRO LUGAR\n");
-		}
-
-		/* Caso um processo tenha extrapolado o tempo, ele deve ser movido para uma fila de nivel mais alto e menor prioridade */
-		if(!solicitouIO && tempoRestante<0 ){
-			printf("colocando processo %d numa fila mais alta\n", id);	
+		if(solicitouTermino){
+			printf("Retirando processo %d (terminado) da fila\n", id);		
 			FILA_remove(fAtual());
-			FILA_insere(fProx(), id, tempo);//coloca numa mais alto	
+			fReset();
+			solicitouTermino = 0;
+			solicitouIO = 0;
 		}
-		/* Caso um processo tenha terminado antes do tempo esperado, ele deve ser movido para uma fila de nivel mais baixo e maior prioridade.
-		Temos que colocar o processo que pediu I/O em uma fila de I/O */
-		else if(solicitouIO && tempoRestante >=0){
-			//FILA_insere(fAnt(), id, tempo);//coloca numa mais baixo	
-			printf("colocando processo %d numa fila mais baixa\n", id);	
-			printf("colocando processo %d em I/O\n", id);
-			FILA_comecaIO(fAtual(), fAnt(), filaIO, tempo);		
-		}
-		/*	Dentro da faixa de 'acabou no tempo certo'
-			Caso o processo tenha terminado no tempo correto, ele é movido para o final da fila 
-		Temos que colocar o processo que pediu I/O em uma fila de I/O */	
-		else if(solicitouIO && tempoRestante<0){
-			//FILA_insere(fAtual(), id, tempo);//coloca na mesma
-			printf("colocando processo %d no final da mesma fila\n", id);
-			printf("colocando processo %d em I/O\n", id);			
-			FILA_comecaIO(fAtual(), fAtual(), filaIO, tempo);
-			
-		}
+		/* Caso um processo tenha extrapolado o tempo, ele deve ser movido para uma fila de nivel mais alto e menor prioridade */
+		else if(!solicitouIO && tempoRestante<0 ){
+			printf("colocando processo %d numa fila mais alta\n", id);
 
-		id = FILA_topId(fAtual());
-		solicitouIO = 0;
+			if(id>0) kill(id, SIGSTOP);
+			else printf("BUG NOUTRO LUGAR\n");
 
-		if(id==-1){
-			printf("fila vazia\n");
-			if(FILA_vazia(filaIO)){
-				printf("Nenhum processo restante, fechando o programa");
-			
-				/* Encerrando... */
-				FILA_libera(f1);
-				FILA_libera(filaIO);
-				exit(0);
+			FILA_remove(fAtual());
+			FILA_insere(fProx(), id, tempo);//coloca numa mais alto
+		}
+		else if(solicitouIO){
+			/*
+				O processo não extrapolou o tempo de execução dele, vamos checar se ele solicitou IO, e qual era o tempo restante quando ele fez isso.
+				Precisamos calcular o tempo restante quando ele pediu IO, e não o tempo restante de agora. */
+			tempoRestante = FILA_tempoRestante(fAtual(), FILA_getTempoComecoIO(fAtual()));
+			/*
+				Caso um processo tenha terminado antes do tempo esperado, ele deve ser movido para uma fila de nivel mais baixo e maior prioridade.
+				Temos que colocar o processo que pediu I/O em uma fila de I/O */
+			if(tempoRestante >0){
+				printf("colocando processo %d numa fila mais baixa\n", id);	
+				printf("colocando processo %d em I/O\n", id);
+				FILA_comecaIO(fAtual(), fAnt(), filaIO, tempo);	//coloca numa mais baixo			
 			}
-		}
-		else{
-			printf("executando processo %d, que e o proximo da fila\n", id);
-			FILA_comecaCPU(fAtual(), tempo);
-
-			if(id>0)
-				kill(id, SIGCONT);
-			else{
-				printf("BUG ACOLA\n");
+			/*	Dentro da faixa de 'acabou no tempo certo'
+				Caso o processo tenha terminado no tempo correto, ele é movido para o final da fila.
+				Temos que colocar o processo que pediu I/O em uma fila de I/O */	
+			else if(tempoRestante<=0){
+				printf("colocando processo %d no final da mesma fila\n", id);
+				printf("colocando processo %d em I/O\n", id);			
+				FILA_comecaIO(fAtual(), fAtual(), filaIO, tempo);//coloca na mesma
 			}
+			solicitouIO = 0;
 		}
+		/*
+			Agora vamos checar se existe algum outro processo numa fila de prioridade maior que está esperando para ser executado.
+			Se houver, precisamos trocar da fila atual para a fila dele.
+		*/
+		/* Fila atual muda para a primeira fila vazia de maior prioridade */
+		/* Se alguma fila alem da filaIO tem um processo aguardando */
+		if(fReset()!=NULL){
+			/* Precisamos continuar o processo atual */
+			id = FILA_topId(fAtual());
+			kill(id, SIGCONT);
+		}
+
+
 	}
 
 	/* Encerrando... */
-	// libera a memória compartilhada do processo
-	shmdt(flag);
-	//libera Fila
-	FILA_libera(f1);
+	encerra(0);
 	return 0;
 }
 
+
+
 /* Funcoes de controle de processos */
 
+/*
 void criaNovoProcesso(ptFila f, char *comando){
 	int id=0;
 	int i=1;
@@ -173,10 +223,11 @@ void criaNovoProcesso(ptFila f, char *comando){
 	}
 	id = fork();
 	if(id==0){
-		printf("criado:%d por: %d - tempo %d - Novo processo\n%s\n", getpid(), getppid(), tempo, comando);
+		printf("criado:%d por: %d - tempo %d - Novo processo id\n%s\n", getpid(), getppid(), tempo, comando);
 		execv("./programa", args);
-		exit(1);
-	}
+		printf("dafuq");
+		encerra(1);
+	}	
 	FILA_insere(f,id, 0);
 	printf("getpid %d parando processo %d\n", getpid(), id);
 	if(id>0)
@@ -184,72 +235,82 @@ void criaNovoProcesso(ptFila f, char *comando){
 	else{
 		printf("BUG AQUI\n");
 	}
-	
 }
+*/
 
 void processoIO(int sinal){
-	printf("Processo %d solicitou IO em tempo %d\n", FILA_topId(fAtual()), tempo);
-	solicitouIO = 1;
+	int id = FILA_topId(fAtual());
+	printf("Processo %d solicitou IO em tempo %d\n", id, tempo);
+	if(id>0){
+		FILA_setTempoComecoIO(fAtual(), tempo);
+		kill(id, SIGSTOP);
+		solicitouIO = 1;
+	}
 }
 
 void processoTermina(int sinal){
 	int id;
-	printf("processo %d terminado\n", FILA_topId(fAtual()));
 	id = FILA_topId(fAtual());
-	FILA_remove(fAtual());
-		
-	//TODO colocar f2 e f3 tambem
-	if(FILA_vazia(filaIO) && FILA_vazia(f1)){
-		printf("todos os processos encerraram, terminando programa\n");
-		
-		/* Encerrando... */
-		FILA_libera(f1);
-		FILA_libera(filaIO);
-		exit(0);
-	}
-	if(id>0)
-		kill(id, SIGCONT);
-	else{
-		printf("BUG ALI\n");
-	}
+	printf("processo %d solicitouTermino em %d\n", id, tempo);
+	solicitouTermino = 1;
 }
 
 /* Funcoes de controle de fila */
 
 ptFila fAtual(){
-//	if(indexAtual<0 ||indexAtual>2) return NULL;
-//	return filas[indexAtual];
-	return f1;
-}
+	//printf("funcFAtual indexAtual: %d\n", indexAtual);
+	if(indexAtual<0 || indexAtual>2) return NULL;
+	return filas[indexAtual];
+}	
 
 /* 
 	Retorna primeira fila nao vazia, atribui index a indexAtual.
 	Retorna NULL se nenhuma fila estiver vazia, atribui -1 a indexAtual.
 */
 ptFila fReset(){
-//	int i;
-//	indexAtual = 0;
-//	for(i=0;i<NFILAS;i++){
-//		if(!FILA_vazia(filas[i])){
-//			indexAtual = i;
-//			return filas[indexAtual];
-//		}
-//	}
-//	indexAtual = -1;
+	
+	int i;
+	printf("funcFreset indexAtual de: %d\n", indexAtual);
+		
+	for(i=0;i<NFILAS;i++){
+		if(!FILA_vazia(filas[i])){
+			indexAtual = i;	
+			printf("funcFreset indexAtual para: %d\n", indexAtual);
+			return filas[i];
+		}
+	}
+
+	if(FILA_vazia(filaIO))
+		encerra(0);
+	
 	return NULL;
 }
 
-/* Retorna fila, um nivel de prioridade maior do que a atual */
+
+
+/* Retorna fila, um nivel de fila maior do que a atual (note que o nivel de fila é inversamente proporcional a prioridade) */
 ptFila fProx(){
-//	int i =  (indexAtual+1)%3;
-//	return filas[i];
-	return f1;
+	int i =  indexAtual+1;
+	//printf("funcFprox indexAtual: %d\n", indexAtual);
+	if(i>NFILAS-1) i = NFILAS-1;
+	return filas[i];
 }
 
-/* Retorna fila, um nivel de prioridade menor do que a atual */
+/* Retorna fila, um nivel de fila menor do que a atual (note que o nivel de fila é inversamente proporcional a prioridade) */
 ptFila fAnt(){
-//	int i = indexAtual-1;
-//	if (i<0)i = NFILAS-1;
-//	return filas[i];
-	return f1;
+	int i = indexAtual-1;
+	//printf("funcFant indexAtual: %d\n", indexAtual);
+	if (i<0) i = 0;
+	return filas[i];
+}
+
+void encerra(int status){
+	printf("encerrando\n");
+	shmdt(flag);
+	shmctl(segmento, IPC_RMID, 0);
+	FILA_libera(f1);
+	FILA_libera(f2);
+	FILA_libera(f3);
+	FILA_libera(filaIO);
+	exit(status);
 }
